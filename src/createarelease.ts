d93350939a4e535e930529546f3f795abb3060ef
jikuma <jikuma@microsoft.com>
2019-08-30 13:30:18 -0700

    adding debug logs

--- src/createarelease.ts
+++ src/createarelease.ts
@@ -9,14 +9,14 @@ export async function run(token) {
     const repoNameWithOwner : string = process.env.GITHUB_REPOSITORY as string;
     var repoNameWithOwnerArray = repoNameWithOwner.split("/", 2); 
     const tag_name = require('./temp_replacevariableininput').getInput('tag_name', {required: true});
-    core.warning("Tagname is "+ tag_name)
+    core.debug("Tagname is "+ tag_name)
     const release_title = core.getInput('release_title', {required: false});
     const release_note = core.getInput('release_note', {required: false});
     const release_draft = core.getInput('release_draft', {required: false});
     const release_prerelease = core.getInput('release_prerelease', {required: false});
     const release_asset_filepath = core.getInput('asset_filepath', {required: false});
     const release_target_commitish: string = process.env.GITHUB_SHA as string;
-    core.warning("Creating release with tag "+ tag_name);
+    core.debug("Creating release with tag "+ tag_name);
     client.repos.createRelease({ owner: repoNameWithOwnerArray[0],
         repo: repoNameWithOwnerArray[1],
         tag_name: tag_name,
@@ -29,7 +29,7 @@ export async function run(token) {
             let rd = fs.createReadStream(release_asset_filepath);
             var stats = fs.statSync(release_asset_filepath);
             let fileName = path.basename(release_asset_filepath);
-            core.warning("Uploading asset "+ release_asset_filepath);
+            core.debug("Uploading asset "+ release_asset_filepath);
             client.repos.uploadReleaseAsset( {
                 url: respose.data.upload_url,
                 headers: {
