ce8426969d1d448c59dbee2728d805a62c71c0e3
jikuma <jikuma@microsoft.com>
2019-08-30 11:21:30 -0700

    fix variabel name

--- src/temp_replacevariableininput.ts
+++ src/temp_replacevariableininput.ts
@@ -6,9 +6,9 @@ export function getInput(name: string, options?: core.InputOptions): string {
         var re = /GITHUB_REF/gi;
         value = value.replace(re, process.env.GITHUB_REF!); 
         var re = /GIHUB_SHORT_SHA/gi;
-        value = value.replace(re, process.env.GIHUB_SHA!.substring(0,7)); 
+        value = value.replace(re, process.env.GITHUB_SHA!.substring(0,7)); 
         var re = /GIHUB_SHA/gi; 
-        return value.replace(re, process.env.GIHUB_SHA!); 
+        return value.replace(re, process.env.GITHUB_SHA!); 
     }
 
     return value;
