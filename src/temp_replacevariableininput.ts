3f64ec33be03d29101c837c95af0884460668141
jikuma <jikuma@microsoft.com>
2019-08-30 13:30:18 -0700

    adding debug logs

--- src/temp_replacevariableininput.ts
+++ src/temp_replacevariableininput.ts
@@ -2,14 +2,11 @@ import * as core from '@actions/core';
 
 export function getInput(name: string, options?: core.InputOptions): string {
     var value = core.getInput(name, options);
-    core.warning(" first "+value);
     if(value){
         var re = /GITHUB_REF/gi;
         value = value.replace(re, process.env.GITHUB_REF!); 
-        core.warning(" second "+value);
         var re = /GITHUB_SHORT_SHA/gi;
         value = value.replace(re, process.env.GITHUB_SHA!.substring(0,7)); 
-        core.warning(" third "+value);
         var re = /GITHUB_SHA/gi; 
         return value.replace(re, process.env.GITHUB_SHA!); 
     }
