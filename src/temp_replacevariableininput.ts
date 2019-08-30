5270f47247326fa5a3351f31eb51aa5d9c4aab14
jikuma <jikuma@microsoft.com>
2019-08-30 11:03:55 -0700

    suport for variable substitution

---
+++ src/temp_replacevariableininput.ts
@@ -0,0 +1,17 @@
+import * as core from '@actions/core';
+
+export function getInput(name: string, options?: core.InputOptions): string {
+    var value = core.getInput(name, options);
+    if(value){
+        var re = /GITHUB_REF/gi;
+        value = value.replace(re, process.env.GITHUB_REF!); 
+        var re = /GIHUB_SHORT_SHA/gi;
+        value = value.replace(re, process.env.GITHUB_REF!.substring(0,7)); 
+        var re = /GIHUB_SHA/gi; 
+        return value.replace(re, process.env.GIHUB_SHA!); 
+    }
+
+    return value;
+}
+
+
